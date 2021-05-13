5b93bf518a9e36e4eda632668ff9c02363b1d508
Koray Kunal <76408353+koraykunal@users.noreply.github.com>
2021-05-13 20:51:53 +0300

    Delete hrms directory

--- hrms/src/main/java/kodlama/io/hrms/dataAcces/abstracts/JobPositionDao.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.hrms.dataAcces.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlama.io.hrms.entities.concrete.JobPosition;
-
-public interface JobPositionDao extends JpaRepository<JobPosition, Integer>{
-
-}
